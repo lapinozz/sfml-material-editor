5d2b0375bd17276e0a340274218508e0f00b5a7f
André Polomat <monstrefou@gmail.com>
2024-05-21 12:17:00 -0400

    Initialize sfml3

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow{ sf::VideoMode{ { 1920u, 1080u } }, "CMake SFML Project" };
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        while (const auto event = window.pollEvent())
+        {
+            if (event.is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+}
