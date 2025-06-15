3f563d8a779f139e7075d9603387bc54a40d1232
lapinozz <monstrefou@gmail.com>
2025-06-15 00:40:18 -0400

    Fixed code gen

--- src/nodes/output.hpp
+++ src/nodes/output.hpp
@@ -20,34 +20,36 @@ struct OutputNode : ExpressionNode
 		if (generator.type == CodeGenerator::Type::Vertex)
 		{
 			auto color = getInput(0);
-			auto alpha = getInput(0);
-
 			if (!color)
 			{
 				color = Value{ Types::scalar, "0.5" };
 			}
 
-			if (!alpha)
-			{
-				alpha = Value{ Types::scalar, "1" };
-			}
-
-
 			generator.shaderInputs.push_back("uniform float time;");
 
-			generator.body.push_back("gl_FrontColor = vec4(" + color.code + ");");
-			generator.body.push_back("gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;");
 			generator.body.push_back("gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;");
+			generator.body.push_back("gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;");
+			generator.body.push_back("gl_FrontColor = vec4(" + color.code + ");");
 		}
 		else if (generator.type == CodeGenerator::Type::Fragment)
 		{
+			const auto color = getInput(0);
+
 			generator.shaderInputs.push_back("uniform sampler2D texture;");
 
 			generator.shaderInputs.push_back("uniform float time;");
 
 			generator.body.push_back("vec4 pixel = texture2D(texture, gl_TexCoord[0].xy);");
 			generator.body.push_back("gl_FragColor = gl_Color * pixel;");
-			generator.body.push_back("gl_FragColor = gl_Color;");
+
+			if (color)
+			{
+				generator.body.push_back("gl_FragColor = vec4(" + color.code + ");");
+			}
+			else
+			{
+				generator.body.push_back("gl_FragColor = gl_Color;");
+			}
 		}
 	}
 
